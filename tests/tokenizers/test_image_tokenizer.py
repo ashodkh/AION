cd1f705c6a7232359e23b91b39a1b49808fed270
Lucas Meyer <lucas.thibaut.meyer@gmail.com>
2025-04-07 22:08:53 +0200

    Add test for MagVitAE image tokenizer

---
+++ tests/tokenizers/test_image_tokenizer.py
@@ -0,0 +1,31 @@
+import pytest
+import torch
+
+from aion.quantizers import FiniteScaleQuantizer
+from aion.tokenizers.image import MagViTAEImageCodec
+
+
+@pytest.mark.parametrize("n_bands", [3, 10])
+@pytest.mark.parametrize("embedding_dim", [5, 10])
+@pytest.mark.parametrize("multisurvey_projection_dims", [12, 24])
+@pytest.mark.parametrize("hidden_dims", [8, 16])
+def test_magvit_image_tokenizer(
+    n_bands, embedding_dim, multisurvey_projection_dims, hidden_dims
+):
+    tokenizer = MagViTAEImageCodec(
+        n_bands=n_bands,
+        quantizer=FiniteScaleQuantizer(levels=[1] * embedding_dim),
+        hidden_dims=hidden_dims,
+        multisurvey_projection_dims=multisurvey_projection_dims,
+        n_compressions=2,
+        num_consecutive=4,
+        embedding_dim=embedding_dim,
+        range_compression_factor=0.01,
+        mult_factor=10,
+    )
+    batch_size = 4
+    random_input = torch.randn(batch_size, n_bands, 96, 96)
+    encoded = tokenizer.encode(random_input)
+    assert encoded.shape == (batch_size, 24, 24)
+    decoded = tokenizer.decode(encoded)
+    assert decoded.shape == random_input.shape
