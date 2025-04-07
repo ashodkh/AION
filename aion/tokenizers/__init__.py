b250bf4ae0eebfbd149f3413ba5b12cd6a50b247
Lucas Meyer <lucas.thibaut.meyer@gmail.com>
2025-04-07 21:54:14 +0200

    Change arborescence for tokenizers

---
+++ aion/tokenizers/__init__.py
@@ -0,0 +1,8 @@
+import torch
+from torch import package
+
+
+def load_tokenizer(path: str, device: str = "cpu") -> torch.nn.Module:
+    importer = package.PackageImporter(path)
+    model = importer.load_pickle("network", "network.pkl", map_location=device)
+    return model
