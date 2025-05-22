a663b96038e6442085627d9e33179648869be317
Lucas Meyer <lucas.thibaut.meyer@gmail.com>
2025-05-22 15:33:49 +0200

    Test scalar tokenizers

--- tests/tokenizers/test_scalar_tokenizer.py
+++ tests/tokenizers/test_scalar_tokenizer.py
@@ -27,12 +27,23 @@ def test_log_reservoir_tokenizer(data_dir, modality):
     )
     codec.eval()
 
-    input_batch = torch.load(data_dir / f"{modality}_input.pt")
-    output_batch = torch.load(data_dir / f"{modality}_output.pt")
+    input_batch = torch.load(
+        data_dir / f"{modality}_codec_input_batch.pt", weights_only=False
+    )
+    reference_encoded_batch = torch.load(
+        data_dir / f"{modality}_codec_encoded_batch.pt", weights_only=False
+    )
+    reference_decoded_batch = torch.load(
+        data_dir / f"{modality}_codec_decoded_batch.pt", weights_only=False
+    )
 
     with torch.no_grad():
         output = codec.encode({modality: input_batch})
-    assert torch.allclose(output, output_batch)
+        decoded_output = codec.decode(output)
+        decoded_output = decoded_output[modality]
+
+    assert torch.allclose(output, reference_encoded_batch)
+    assert torch.allclose(decoded_output, reference_decoded_batch)
 
 
 @pytest.mark.parametrize("modality", ["SHAPE_E1", "SHAPE_E2", "EBV"])
@@ -41,11 +52,21 @@ def test_reservoir_tokenizer(data_dir, modality):
         f"polymathic-ai/aion-scalar-{modality.lower().replace('_', '-')}-codec"
     )
     codec.eval()
-    codec.load_state_dict(torch.load(data_dir / f"{modality}_codec.pt"))
 
-    input_batch = torch.load(data_dir / f"{modality}_input.pt")
-    output_batch = torch.load(data_dir / f"{modality}_output.pt")
+    input_batch = torch.load(
+        data_dir / f"{modality}_codec_input_batch.pt", weights_only=False
+    )
+    reference_encoded_batch = torch.load(
+        data_dir / f"{modality}_codec_encoded_batch.pt", weights_only=False
+    )
+    reference_decoded_batch = torch.load(
+        data_dir / f"{modality}_codec_decoded_batch.pt", weights_only=False
+    )
 
     with torch.no_grad():
         output = codec.encode({modality: input_batch})
-    assert torch.allclose(output, output_batch)
+        decoded_output = codec.decode(output)
+        decoded_output = decoded_output[modality]
+
+    assert torch.allclose(output, reference_encoded_batch)
+    assert torch.allclose(decoded_output, reference_decoded_batch)
