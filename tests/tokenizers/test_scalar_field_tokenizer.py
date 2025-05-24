6a944f0754c887f5b4208fb571940c347e1c487d
EiffL <fr.eiffel@gmail.com>
2025-05-24 17:14:30 -0400

    adding test for scalar field tokenizer

---
+++ tests/tokenizers/test_scalar_field_tokenizer.py
@@ -0,0 +1,26 @@
+import torch
+
+from aion.codecs import ScalarFieldCodec
+from aion.modalities import ScalarField
+
+
+def test_scalar_field_tokenizer(data_dir):
+    codec = ScalarFieldCodec.from_pretrained("polymathic-ai/aion-scalar-field-codec")
+
+    input_batch = torch.load(
+        data_dir / "scalar-field_codec_input_batch.pt", weights_only=False
+    )
+    reference_encoded_batch = torch.load(
+        data_dir / "scalar-field_codec_encoded_batch.pt", weights_only=False
+    )
+    reference_decoded_batch = torch.load(
+        data_dir / "scalar-field_codec_decoded_batch.pt", weights_only=False
+    )
+
+    with torch.no_grad():
+        output = codec.encode(ScalarField(field=input_batch))
+        decoded_output = codec.decode(output)
+
+    assert torch.allclose(output, reference_encoded_batch)
+    assert torch.allclose(decoded_output.field, reference_decoded_batch, atol=1e-4, rtol=1e-4)
+    
